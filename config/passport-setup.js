7aee9a0e342552837b16fbfc0b8b03a0ec59570e
RishabhKr bothra <rbothra77@gmail.com>
2018-03-30 22:07:44 +0530

    final commit

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,17 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
 const User = require('../models/user-model');
 
+passport.serializeUser((user,done)=>{
+    done(null,user.id)
+});
+
+passport.deserializeUser((id,done)=>{
+    User.findById(id).then((user)=>{
+        done(null,user)
+    })
+
+});
+
 passport.use(
     new GoogleStrategy({
         // options for google strategy
@@ -12,17 +23,20 @@ passport.use(
     } ,(accessToken, refreshToken, profile, done) => {
         // passport callback function
         //console.log('passport callback function fired:');
-        //console.log(profile);
+        console.log(profile);
         User.findOne({googleId:profile.id}).then((currentUser)=>{
             if(currentUser){
-                console.log('currentUser :'+ currentUser)
+                console.log('currentUser :'+ currentUser);
+                done(null,currentUser);
             }
             else{
                 new User({
                     googleId: profile.id,
-                    username: profile.displayName
+                    username: profile.displayName,
+                    thumbnail:profile._json.image.url
                 }).save().then((newUser) => {
                     console.log('new user created: ', newUser);
+                    done(null,newUser);
                 });
             }
         })
