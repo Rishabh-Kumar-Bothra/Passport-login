cd6afcf696f2a1eb852107bb3a2be227f8b2dfc8
RishabhKr bothra <rbothra77@gmail.com>
2018-03-30 13:38:28 +0530

    mongodb added

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express');
-const authRoutes = require('./routes/auth-routes')
+const authRoutes = require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
+const mongoose =  require('mongoose');
+const keys = require('./config/keys');
+
 const app = express();
 
-app.set('view engine','ejs');
+// set view engine
+app.set('view engine', 'ejs');
 
-
-app.use('/auth',authRoutes);
-
-
-app.get('/',(req,res)=>{
-    res.render('home');
+//mongodb connection
+mongoose.connect(keys.mongodb.dbURI,()=>{
+    console.log('connected to db');
 })
 
 
+// set up routes
+app.use('/auth', authRoutes);
 
+// create home route
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
-
-app.listen(3030);
+app.listen(3000, () => {
+    console.log('app now listening for requests on port 3000');
+});
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,9 +1,23 @@
 const passport = require('passport');
-const GoogleStatergy = require('passport-google-oauth20');
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
-    new GoogleStatergy({
-
+    new GoogleStrategy({
+        // options for google strategy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret,
+        callbackURL: '/auth/google/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        //console.log('passport callback function fired:');
+        //console.log(profile);
+        new User({
+            googleId: profile.id,
+            username: profile.displayName
+        }).save().then((newUser) => {
+            console.log('new user created: ', newUser);
+        });
     })
-)
+);
