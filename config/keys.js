68c98c4b9a91c17ad601c7908beca21b889658a6
RishabhKr bothra <rbothra77@gmail.com>
2018-04-02 17:02:58 +0530

    testing again

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        clientID:'249880920428-igci24mcb9t6hgeruii2ia7rtpmeoapd.apps.googleusercontent.com',
+        clientSecret:'WHATroEmYCbcXp2a0BNLhdwv'
+    },
+    mongodb:{
+        dbURI:'mongodb://test:testing@ds121889.mlab.com:21889/oauth-test'
+    },
+    session:{
+        cookieKey:'baapbaaphotahai'
+    }
+};
